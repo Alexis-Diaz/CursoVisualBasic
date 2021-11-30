4ded64229bbf271ce00ff32c68028296da441413
Alexis-Diaz <alex.d4556@gmail.com>
2021-11-30 11:12:43 -0600

    Bucles

---
+++ DoLoop(DoWhile)/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        'El bucle do loop siempre se repetirá minimo una vez
+        'existen dos formas: con while y con until
+
+        'el while repite mientras condicion sea true
+        'el until repite mientras condicion sea false
+        Dim numero As Integer
+        Do
+            Console.WriteLine("Do Loop While entra si evalua true")
+            Console.WriteLine("Ingrese un numero-->")
+            numero = Console.ReadLine()
+
+        Loop While (numero <> 0)
+
+        Do
+            Console.WriteLine("Do Loop Until entra si evalua false")
+
+            Console.WriteLine("Ingrese un numero-->")
+            numero = Console.ReadLine()
+        Loop Until (numero <> 0)
+
+
+
+    End Sub
+
+End Module

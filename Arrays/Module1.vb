520016acb5ecb92733d90c59e5d4af2c8edbed56
Alexis-Diaz <alex.d4556@gmail.com>
2022-04-06 10:24:04 -0600

    Ultimos cambios

---
+++ Arrays/Module1.vb
@@ -0,0 +1,57 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'Declaracion de un array
+        'Lo que va entre parentesis es el numero de elementos que contendra
+
+        '1 Forma: inicializando el array
+        Dim arrayNombres(6) As String
+
+        'ASIGNAR VALORES
+
+        '1 Forma: desde la declaracion
+        Dim arrayApellidos = New String() {"Franco", "Lue", "Martinez", "Cordova"}
+
+        '2 Forma: usando un bucle y asignado valores por medio de su indice
+        'For i = 0 To arrayNombres.Length - 1 Step 1
+        '    Console.WriteLine("Ingrese el valor de la posicion " & i & "==>")
+        '    arrayNombres(i) = Console.ReadLine()
+        'Next
+
+        '3 Forma: sin usar bucle y asignando valores por medio de su indice
+        arrayApellidos(0) = "Rivera"
+        arrayApellidos(1) = "Hernandez"
+        arrayApellidos(2) = "Samayoa"
+        arrayApellidos(3) = "Ortiz"
+
+        'ACCEDER A SUS VALORES
+        Console.WriteLine(arrayApellidos(2)) 'salida Samayoa
+
+        Dim apellido As String = arrayApellidos(3) 'almacenamos en una variable el apellido Ortiz
+        Console.WriteLine(apellido) 'salida Samayoa
+
+
+
+        'RECORRIDO DE ARRAY
+        'Usando for
+        Console.WriteLine()
+        Console.WriteLine("for")
+
+        For i = 0 To arrayApellidos.Length - 1 Step 1
+            Console.WriteLine(arrayApellidos(i))
+        Next
+
+
+        'Usando foreach
+        Console.WriteLine()
+        Console.WriteLine("for each")
+
+        For Each item As String In arrayApellidos
+            Console.WriteLine(item)
+
+        Next
+
+    End Sub
+
+End Module
---
+++ BuclesAnidados/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        'BUCLES ANIDADOS
+        'Se usan mas con los bucles for
+
+        For i = 1 To 2 Step 1
+            Console.WriteLine("Este es el mensaje del primer for")
+            For j = 1 To 3 Step 1
+                Console.WriteLine("Este es el mensaje del segundo for")
+
+            Next
+        Next
+
+
+    End Sub
+
+End Module

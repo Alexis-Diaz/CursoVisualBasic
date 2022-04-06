f7f89ffd1d84627638225140b58661a20b51d1a5
Alexis-Diaz <alex.d4556@gmail.com>
2022-04-06 14:33:31 -0600

    Variables, constantes, ambito, operadores

---
+++ AmbitoDeVariables/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+    'El ambito o scope hacer referencia al alcance de la variable
+
+    'Alcance global: este tipo de variables seran alcanzadas desde todos los metodos que pertenecen
+    'a la clase Module1
+
+    Dim VariableGlobal As String = "Osmin"
+
+
+    Sub Main()
+        Dim VariableLocal As String = "Alexis"
+
+        Console.WriteLine(VariableGlobal)
+        Console.WriteLine(VariableLocal)
+
+        Metodo()
+        Console.ReadKey()
+
+
+    End Sub
+
+
+    Sub Metodo()
+        Console.WriteLine(VariableGlobal)
+        'Console.WriteLine(VariableLocal)'No es alcanzable
+    End Sub
+End Module

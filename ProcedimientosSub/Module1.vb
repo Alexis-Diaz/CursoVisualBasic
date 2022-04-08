6864cea1e4f081330d152d855f1244717180728e
Alexis-Diaz <alex.d4556@gmail.com>
2022-04-08 14:27:17 -0600

    Metodos o procedimientos Sub

---
+++ ProcedimientosSub/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    'Los procedimientos sub no tienen valor de retorno
+    'pero pueden recibir parametros
+    'El llamado se realiza como cualquier otro metodo C#
+
+    Sub Main()
+        Console.WriteLine("Procedimiento principal")
+        mostrarMensaje()
+    End Sub
+
+    Sub mostrarMensaje()
+        Console.WriteLine("Estamos dentro del procedimiento sub")
+    End Sub
+
+End Module
---
+++ Sub/Conceptos.vb
@@ -0,0 +1,25 @@
+﻿Module Conceptos
+
+    'En visual basic.Net, tenemos varios tipos de procedimientos:
+
+    '1- Sub
+    '2- Function
+    '3- Property
+    '4- Operators
+
+    'SUB
+    'Los procedimientos sub son similares a las funciones vacias o sin valores de retorno
+    'de lenguajes como C#, mantenimiento la misma funcionalidad. Estas realizan una operacion
+    ' en especifico pero sin retornar valores. 
+    'Consideracion a tomar en cuenta:
+
+    '* Estos procedimientos comienzan con la palabra Sub y terminan con la palabra reservada
+    'End Sub.
+
+    '* No deben retornar ningun valor
+
+    '* Pueden tener o no argumentos
+
+    '''''
+
+End Module

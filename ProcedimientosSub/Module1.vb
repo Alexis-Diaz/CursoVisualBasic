9b28c589c1178522bbe23fa8bb4624ef4297ab15
Alexis-Diaz <alex.d4556@gmail.com>
2022-04-08 14:34:33 -0600

    Procedimientos Sub y parametros

--- ProcedimientosSub/Module1.vb
+++ ProcedimientosSub/Module1.vb
@@ -1,16 +1,31 @@
 ﻿Module Module1
 
+    'FUNCIONES O METODOS SUB
     'Los procedimientos sub no tienen valor de retorno
     'pero pueden recibir parametros
     'El llamado se realiza como cualquier otro metodo C#
 
     Sub Main()
         Console.WriteLine("Procedimiento principal")
+
+        'Llamadas a otros metodos
         mostrarMensaje()
+        saludo("Alexis")
+
     End Sub
 
+    'FUNCIONES SIN PARAMETROS
     Sub mostrarMensaje()
         Console.WriteLine("Estamos dentro del procedimiento sub")
     End Sub
+
+
+    'FUNCIONES CON PARAMETROS
+    Sub saludo(nom As String)
+
+        Console.WriteLine("Hola " & nom & " bienvenido(a) al mejor curso de visual basic.NET")
+
+    End Sub
+
 
 End Module

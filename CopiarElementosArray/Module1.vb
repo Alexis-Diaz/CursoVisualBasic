853cc74a51cabbb3fd03a1f38be08b45bccc90f4
Alexis-Diaz <alex.d4556@gmail.com>
2022-04-08 10:21:55 -0600

    Copia de array

--- CopiarElementosArray/Module1.vb
+++ CopiarElementosArray/Module1.vb
@@ -2,6 +2,25 @@
 
     Sub Main()
 
+        'Tres cosas a tomar en cuenta cuando se copia un array
+
+        '1- La variable destino debe tener el mismo tamaño
+        '2- La variable destino debe ser del mismo tipo
+        '3- La variable destino debe ser array vacio (esto no es necesario)
+
+
+        Dim numeros = New Integer() {14, 28, 56}
+
+        Dim arrayCopia(2) As Integer  'Como el cero se cuenta se se usa 2 en el parentesis para que sean 3 elementos
+
+        For i = 0 To 2 Step 1
+            arrayCopia(i) = numeros(i)
+        Next
+
+        For i = 0 To arrayCopia.Length - 1 Step 1
+            Console.Write(arrayCopia(i) & " ")
+        Next
+
     End Sub
 
 End Module

0b2d0dd8bc3f0deea78acd5f5330248138c8a234
Alexis-Diaz <alex.d4556@gmail.com>
2022-04-08 10:07:04 -0600

    Ordenamientos por inserccion y burbuja

---
+++ OrdenamientoPorInserccion/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        'El siguiente codigo ordena el array de menor a mayor. Basicamente lo que hace es recorrer el array
+        'y comparar cada elemento con su anterior, si resulta que es menor cambian de posicion hasta que 
+        'el array queda completamente ordenado.
+
+        Dim numeros = New Integer() {233, 301, 192, 169, 215}
+
+        For i = 1 To numeros.Length - 1 Step 1
+            For j = i To 1 Step -1
+                If numeros(j) < numeros(j - 1) Then
+
+                    Dim aux As Integer = numeros(j)
+                    numeros(j) = numeros(j - 1)
+                    numeros(j - 1) = aux
+
+                End If
+            Next
+
+        Next
+        For Each num As Integer In numeros
+            Console.Write(num & " ")
+        Next
+    End Sub
+
+End Module
---
+++ OrdenamientoPorMetodoDeBurbuja/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim numeros = New Integer() {233, 301, 192, 169, 215}
+
+        For i = 0 To numeros.Length - 1 Step 1
+            For j = 0 To numeros.Length - 2 Step 1
+
+                If numeros(j) > numeros(j + 1) Then
+
+                    Dim aux As Integer = numeros(j)
+                    numeros(j) = numeros(j + 1)
+                    numeros(j + 1) = aux
+
+                End If
+            Next
+        Next
+
+        For Each num As Integer In numeros
+
+            Console.Write(num & " ")
+
+        Next
+    End Sub
+
+End Module

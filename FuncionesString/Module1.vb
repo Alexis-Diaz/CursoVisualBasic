2481f7162869d8cdfbc4dfe1989f97f5100db379
Alexis-Diaz <alex.d4556@gmail.com>
2022-04-12 13:58:43 -0600

    Funciones de la clase string

---
+++ FuncionesString/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        'El tippo string es una clase, los objetos creados con esta clase
+        'tienen metodos que resultan utilies al programar
+        Dim cadena1 = "Hola mi nombre es Alexis"
+        Dim cadena2 = "hola mi nombre es alexis"
+
+
+        'La clase String tiene funciones. Algunas de ellas son:
+
+        'ToUpper
+        Console.WriteLine(cadena1.ToUpper)
+
+        'ToLower
+        Console.WriteLine(cadena1.ToLower)
+
+        'SubString
+        Console.WriteLine(cadena1.Substring(0, 4)) 'output: Hola
+
+        'Length
+        Console.WriteLine(cadena1.Length)
+
+        'Equals
+        Console.WriteLine(cadena1.Equals(cadena2)) 'output: false
+    End Sub
+
+End Module

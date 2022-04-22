53e702cc39768d64b45e20f5a1668ad8361c4dd3
Alexis-Diaz <alex.d4556@gmail.com>
2022-04-22 14:11:13 -0600

    Constructores de clase

---
+++ MetodoConstructor/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        'Los metodos constructores sirven para darle un estado inicial
+        'al objeto creado. Eso lo logramos cuando realizamos la instancia
+        'de una clase.
+
+        Dim Sams As AireAcondicionado = New AireAcondicionado("Freezer", "blanco", 220, 18000)
+
+        Console.WriteLine(Sams.marca)
+        Console.WriteLine(Sams.modelo)
+        Console.WriteLine(Sams.color)
+        Console.WriteLine(Sams.voltaje)
+        Console.WriteLine(Sams.btu)
+        Console.WriteLine(Sams.temperatura)
+        Console.ReadKey()
+
+    End Sub
+
+End Module

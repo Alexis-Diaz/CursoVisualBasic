700f33916ce55415d44bb3d1739c7e847d0c69bb
Alexis-Diaz <alex.d4556@gmail.com>
2022-04-25 16:56:09 -0600

    Shared (propiedades y métodos estáticos)

---
+++ Shared/Estudiante.vb
@@ -0,0 +1,33 @@
+﻿Public Class Estudiante
+    'SHARED
+    'Esta palabra nos permite declarar una variable, propiedad, función o método
+    'y asociarlo solo a la clase y no la instancia.
+
+    'Su implementación es la siguiente: 
+    ' * En el caso de las variables o atributos de clase que implementen esta palabra clave
+    'el valor solo será de la clase y no de la instancia u objeto.
+
+    '* En el caso de los metodos, para utilizarlo no se debe crear una instancia de la clase
+    'para acceder a ellos, sino que se coloca el nombnre de la clase seguido del metodo 
+    '(esto es equivalente a los metodos o propiedades estaticas de C#)
+
+
+    Private codigoEstudiante As Integer
+    Private nombre As String
+
+    Private Shared codigo As Integer = 1
+
+    Public Sub New(nom As String)
+        nombre = nom
+        codigoEstudiante = codigo
+        codigo += 1
+    End Sub
+
+    Public Function getCodigoEstudiante() As Integer
+        Return codigoEstudiante
+    End Function
+
+    Public Function getNombre() As String
+        Return nombre
+    End Function
+End Class
---
+++ Shared/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+        'Al crear dos instancias de la clase Estudiante, el constructor
+        'aumenta en 1 la variable o atributo codigo, como este es una
+        'variable estatica su valor se comparte y es el mismo para todas
+        'las instancias.
+
+        Dim estudiante1 As Estudiante = New Estudiante("Alexis Diaz")
+
+        Dim estudiante2 As Estudiante = New Estudiante("Osmin Lue")
+
+        Dim estudiante3 As Estudiante = New Estudiante("Adalberto Lue")
+
+        Console.WriteLine(estudiante1.getCodigoEstudiante)
+        Console.WriteLine(estudiante2.getCodigoEstudiante)
+        Console.WriteLine(estudiante3.getCodigoEstudiante)
+
+
+        Console.WriteLine(Math.Pow(5, 3))
+        Console.WriteLine(Matematica.potenciacion(5, 3))
+    End Sub
+
+End Module

e6599119409e6d8fc20b638c585ef3a079ccb441
Alexis-Diaz <alex.d4556@gmail.com>
2022-11-15 14:31:43 -0600

    Asociaciones en visual basic

---
+++ Asociaciones/CuentaAhorros.vb
@@ -0,0 +1,63 @@
+﻿Public Class CuentaAhorros
+
+    'Campos
+    Private numeroCuentaAhorros As Integer
+    Private saldo As Double
+    Private cte As Cliente
+
+    Private Shared estableceNumeroCuenta As Integer = 1
+
+    'Constructores
+    Public Sub New()
+        numeroCuentaAhorros = estableceNumeroCuenta
+        saldo = 0
+        cte = Nothing
+
+        estableceNumeroCuenta += 1
+    End Sub
+
+    Public Sub New(cl As Cliente, sa As Double)
+        numeroCuentaAhorros = estableceNumeroCuenta
+        saldo = sa
+        cte = cl
+
+        estableceNumeroCuenta += 1
+    End Sub
+
+    'Propiedades
+    Public Property clienteCuenta() As Cliente
+        Get
+            Return cte
+        End Get
+        Set(value As Cliente)
+            If (cte Is Nothing) Then
+                cte = value
+            Else
+                Console.WriteLine("El cliente ya esta asignado a la cuenta")
+            End If
+
+        End Set
+    End Property
+
+    'Metodos
+    Public Function getNumeroCuentaAhorros() As Integer
+        Return numeroCuentaAhorros
+    End Function
+
+    Public Function getSaldo() As Double
+        Return saldo
+    End Function
+
+    Public Sub depositarDinero(va As Double)
+        saldo = saldo + va
+    End Sub
+
+    Public Sub retirarDinero(va As Double)
+        If (saldo >= va) Then
+            saldo = saldo - va
+        Else
+            Console.WriteLine("El saldo disponible es menor al valor a retirar")
+        End If
+
+    End Sub
+End Class
---
+++ Asociaciones/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        'La asociación se da cuando una clase es un campo en otra clase. En este ejemplo
+        'la clase Cliente es un campo en la clase CuentaAhorro
+
+        Dim cuenta1 As New CuentaAhorros(New Cliente("Alexis", "123", "calle tal, ciudad tal", "12345678"), 250)
+        Dim cuenta2 As New CuentaAhorros(New Cliente("Osmin", "123", "calle tal, ciudad tal", "12345678"), 250)
+
+        Console.WriteLine(cuenta1.getSaldo())
+        cuenta1.depositarDinero(600)
+        Console.WriteLine(cuenta1.getSaldo())
+        cuenta1.retirarDinero(400)
+        Console.WriteLine(cuenta1.getSaldo)
+
+        Console.WriteLine(cuenta1.clienteCuenta.NombreCliente)
+        Console.WriteLine(cuenta1.getNumeroCuentaAhorros)
+        Console.WriteLine(cuenta2.getNumeroCuentaAhorros)
+    End Sub
+
+End Module

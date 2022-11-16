336aad01939bcf4b001fda468b2345be7deef072
Alexis-Diaz <alex.d4556@gmail.com>
2022-11-16 13:21:00 -0600

    Array de objetos

---
+++ ArrayObjetos/Module1.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'Los array de objetos son como cualquier otros array pero a diferencia de usar
+        'datos primitivos usamos objetos. A continuacion Se crea un array de la clase Persona
+        'con una cantidad de 3 elementos. Como el cero cuenta, entre paréntesis
+        'se escribe 2
+
+        Dim personas(2) As Persona
+        Dim nombre As String
+        Dim apellido As String
+        'Creación de los objetos
+        For i = 0 To personas.Length - 1 Step 1
+
+            Console.WriteLine("Ingrese el nombre de la " & i + 1 & "º persona")
+            nombre = Console.ReadLine
+
+            Console.WriteLine("Ingrese el apellido de la " & i + 1 & "º persona")
+            apellido = Console.ReadLine
+
+            Dim persona As Persona = New Persona()
+            persona.NombrePersona = nombre
+            persona.ApellidoPersona = apellido
+            personas(i) = persona
+
+        Next
+
+        Console.WriteLine()
+
+        'Lectura del array
+        For Each persona As Persona In personas
+            Console.WriteLine(persona.NombrePersona)
+        Next
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ ArrayObjetos/Persona.vb
@@ -0,0 +1,22 @@
+﻿Public Class Persona
+    Private nombre As String
+    Private apellido As String
+
+    Public Property NombrePersona As String
+        Get
+            Return nombre
+        End Get
+        Set(value As String)
+            nombre = value
+        End Set
+    End Property
+
+    Public Property ApellidoPersona As String
+        Get
+            Return apellido
+        End Get
+        Set(value As String)
+            apellido = value
+        End Set
+    End Property
+End Class

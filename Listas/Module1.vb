d695bb9535209d3d648fed978652b53be89ed112
Alexis-Diaz <alex.d4556@gmail.com>
2022-11-21 17:37:26 -0600

    Listas, asignación de valores y recorridos

--- Listas/Module1.vb
+++ Listas/Module1.vb
@@ -2,27 +2,75 @@
 
     Sub Main()
 
-        'Creacion de un lista
-        Console.WriteLine("Creacion de una lista")
-        Dim miLista As New List(Of Integer)
+        'LISTAS
 
-        'Se agregan elementos a la lista con el metodo Add
-        miLista.Add(48)
-        miLista.Add(30)
-        miLista.Add(120)
-        miLista.Add(92)
+        '************Creacion de un lista con tipos primitivos************
+
+        'Declaracion de una lista de string
+
+        Dim _miLista1 As List(Of String) = New List(Of String)
+
+        'Adición de los elementos con el método Add
+        _miLista1.Add("Alexis")
+        _miLista1.Add("Zapatos")
+        _miLista1.Add("Perfume")
+
+
+        Dim miLista1 As New List(Of String)
+
+        'Adición de los elementos con el método Add
+        miLista1.Add("Alexis")
+        miLista1.Add("Zapatos")
+        miLista1.Add("Perfume")
+
+
+
+
+
+
+
+        'Declaración y asignación de los elementos de la lista
+        Dim _miLista2 As List(Of String) = New List(Of String) From {"Ventilador", "Carne", "Cepillo"}
+
+        Dim miLista2 As List(Of String) = New List(Of String) From {
+            "Ventilador",
+            "Carne",
+            "Cepillo"
+        }
+
+
+        'Declaración y asignación de los elementos de la lista abreviado
+        Dim _miLista3 As New List(Of Integer) From {23, 34, 56, 67}
+
+        Dim miLista3 As New List(Of Integer) From {
+            23,
+            34,
+            56,
+            67
+        }
+
+
+        'Recorridos de las listas
+
+        Dim miLista4 As New List(Of Integer)
+
+        miLista4.Add(48)
+        miLista4.Add(30)
+        miLista4.Add(120)
+        miLista4.Add(92)
 
         'Recorrido de la lista con el bucle For
-        For i = 0 To miLista.Count - 1 Step 1
-            Console.WriteLine(miLista(i))
+        For i = 0 To miLista4.Count - 1 Step 1
+            Console.WriteLine(miLista4(i))
         Next
 
         Console.WriteLine()
 
         'Recorrido de la lista con el bucle For Each
-        For Each item As Integer In miLista
+        For Each item As Integer In miLista4
             Console.WriteLine(item)
         Next
+
 
 
 
@@ -30,7 +78,7 @@
         Console.WriteLine()
         Console.WriteLine("Copia de lista en Array")
 
-        Dim copiaLista() As Integer = miLista.ToArray
+        Dim copiaLista() As Integer = miLista4.ToArray
 
         For Each item As Integer In copiaLista
 
@@ -39,10 +87,10 @@
 
         'Eliminacion de todos los elementos de la lista
         Console.WriteLine("Se eliminan los elementos de la lista")
-        miLista.Clear()
+        miLista4.Clear()
 
-        For i = 0 To miLista.Count - 1 Step 1
-            Console.WriteLine(miLista(i))
+        For i = 0 To miLista4.Count - 1 Step 1
+            Console.WriteLine(miLista4(i))
         Next
 
 

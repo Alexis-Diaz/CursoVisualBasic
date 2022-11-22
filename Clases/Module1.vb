d625db812c628a5f918000f63ded07c8aaefa447
Alexis-Diaz <alex.d4556@gmail.com>
2022-11-22 12:16:55 -0600

    Clases y objetos

--- Clases/Module1.vb
+++ Clases/Module1.vb
@@ -1,29 +1,74 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim objeto As AirAcondicionado = New AirAcondicionado()
+        'CLASES
 
-        objeto.marca = "Hyu"
-        objeto.modelo = "MH-92"
-        objeto.color = "color"
-        objeto.voltaje = "110"
-        objeto.btu = 18000
-        objeto.temperatura = 0
+        'Diferentes formas de crear un objeto
 
-        Console.WriteLine(objeto.marca)
-        Console.WriteLine(objeto.modelo)
-        Console.WriteLine(objeto.color)
-        Console.WriteLine(objeto.voltaje)
-        Console.WriteLine(objeto.btu)
-        Console.WriteLine(objeto.temperatura)
+        '1) Instanciandolo y agregando valores a sus propiedades
+        '   por medio de punto
+        Dim objeto1 As AirAcondicionado = New AirAcondicionado()
+        objeto1.marca = "Hyu"
+        objeto1.modelo = "MH-92"
+        objeto1.color = "color"
+        objeto1.voltaje = "110"
+        objeto1.btu = 18000
+        objeto1.temperatura = 0
+
+        Dim objeto2 As New AirAcondicionado()
+        objeto2.marca = "Hyu"
+        objeto2.modelo = "MH-92"
+        objeto2.color = "color"
+        objeto2.voltaje = "110"
+        objeto2.btu = 18000
+        objeto2.temperatura = 0
+
+
+
+        '2) Instanciandolo y agregando valores a sus propiedades
+        '   en la misma inicialización
+        Dim objeto3 As AirAcondicionado = New AirAcondicionado With {
+            .marca = "Hyu",
+            .modelo = "MH-92",
+            .color = "color",
+            .voltaje = "110",
+            .btu = 18000,
+            .temperatura = 0
+        }
+
+        Dim objeto4 As New AirAcondicionado With {
+            .marca = "Hyu",
+            .modelo = "MH-92",
+            .color = "color",
+            .voltaje = "110",
+            .btu = 18000,
+            .temperatura = 0
+        }
+
+
+
+        '2) Instanciandolo y agregando valores a sus propiedades
+        '   por medio del constructor. Falta agregar el ejemplo
+
+
+
+
+
+        'Lectura de los valores
+        Console.WriteLine(objeto1.marca)
+        Console.WriteLine(objeto1.modelo)
+        Console.WriteLine(objeto1.color)
+        Console.WriteLine(objeto1.voltaje)
+        Console.WriteLine(objeto1.btu)
+        Console.WriteLine(objeto1.temperatura)
 
         'Uso del metodo de temperatura
         Console.WriteLine()
         Console.WriteLine("Subiendo temperatura")
-        objeto.subirTemperatura()
-        objeto.subirTemperatura()
-        objeto.subirTemperatura()
-        Console.WriteLine(objeto.temperatura)
+        objeto1.subirTemperatura()
+        objeto1.subirTemperatura()
+        objeto1.subirTemperatura()
+        Console.WriteLine(objeto1.temperatura)
     End Sub
 
 End Module
